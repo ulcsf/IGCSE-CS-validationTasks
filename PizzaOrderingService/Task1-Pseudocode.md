7ef0751409abfc237aa07e68eb1e412aa3106c7a
Si <Si@geeky-teacher.co.uk>
2019-10-13 18:26:39 +0800

    Taskk one modifications

---
+++ PizzaOrderingService/Task1-Pseudocode.md
@@ -0,0 +1,44 @@
+# Pizza Ordering Service
+
+## Pseudocode
+
+PRINT("Welcome to the Pizza Ordering Service")
+
+PRINT("Please select the type of base you want: ")
+
+customerBase <-- INPUT("THICK or THIN: ")
+
+PRINT("Thank you.)
+
+PRINT("Please select the size of pizza you want: ")
+
+customerSize <-- INPUT("SMALL, MEDIUM, LARGE: ")
+
+PRINT("Thank you.)
+
+PRINT("Please pick 3 additional toppings from the list:")
+
+PRINT("TOPPINGS:")
+PRINT("Pepperoni - Chicken - Extra cheese - Mushrooms - Spinach - Olives")
+
+toppingChoice1 <-- INPUT("Enter choice 1: ")
+
+toppingChoice2 <-- INPUT("Enter choice 2: ")
+
+toppingChoice3 <-- INPUT("Enter choice 3: ")
+
+PRINT("Thank you")
+
+PRINT("Your order is:")
+
+PRINT("Base:",customerBase)
+
+PRINT("Size:",customerSize)
+
+PRINT("Toppings:")
+
+PRINT(toppingChoice1)
+
+PRINT(toppingChoice2)
+
+PRINT(toppingChoice3)
---
+++ PizzaOrderingService/Task1-outline_B.py
@@ -0,0 +1,38 @@
+'''
+TASK 1 – Design your pizza. 
+Part A: 
+The customer is given choices of size, base and additional toppings (number and type) as stated above. 
+Only valid choices can be accepted. 
+
+Part B:
+The customer is asked to confirm their order or alter their 
+choices or not proceed. If the customer confirms their order they are given a unique order number.
+--------------------------------------
+
+'''
+#Lists containing the options for the customer
+toppings = ["Pepperoni","Chicken","Extra cheese","Mushrooms","Spinach","Olives"]
+baseSizes = ["Small","Medium","Large"]
+baseTypes = ["Thin","Thick"]
+
+#NOTE: No other variable have been created. You may require more than this!!
+
+print("""
+Hi, welcome to the Pizza Ordering Service.
+
+Please follow the on-screen prompts to place your order.
+
+Press return/enter to continue...
+""")
+input()
+
+
+  
+
+    
+    
+
+
+print("""
+Thank you!
+Your order number is: """)
