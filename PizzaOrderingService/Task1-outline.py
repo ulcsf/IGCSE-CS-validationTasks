39ea26f311c9e49dcce6d8811b5288709677ef70
Si <Si@geeky-teacher.co.uk>
2019-10-13 13:38:41 +0800

    task 1 outline created

---
+++ PizzaOrderingService/Task1-outline.py
@@ -0,0 +1,58 @@
+'''
+TASK 1 – Design your pizza. 
+Part A: 
+The customer is given choices of size, base and additional toppings (number and type) as stated above. 
+Only valid choices can be accepted. 
+
+Part B:
+The customer is asked to confirm their order or alter their 
+choices or not proceed. If the customer confirms their order they are given a unique order number.
+--------------------------------------
+
+'''
+#Lists containing the options for the customer
+toppings = ["Pepperoni","Chicken","Extra cheese","Mushrooms","Spinach","Olives"]
+baseSizes = ["Small","Medium","Large"]
+baseTypes = ["Thin","Thick"]
+
+#NOTE: No other variable have been created. You may require more than this!!
+
+#**START**
+
+#TODO: 1. Display a 'welcome' message to the user
+
+print("""
+Hi, welcome to the Pizza Ordering Service.
+
+Please follow the on-screen prompts to place your order.
+
+Press return/enter to continue...
+""")
+input()
+
+#TODO: 2. Ask the user what size base they want and store the answer.
+#HINT:      Show the user the options they have available and ask them to repond. Are there any problems that could happen?
+
+
+
+#TODO: 3. Ask the user to choose a type of pizza base and store the answer
+
+
+
+#TODO: 4. Show the customer the extra toppings available. Ask the user to  
+
+
+
+#TODO: 5. Output the order so the user can check their choices.
+    print("""Thank you. 
+    Please review your choices and confirm your order: 
+
+    *****YOUR ORDER ****""")
+  
+
+#TODO: 6. Ask the user if their order is correct. If it isn't...let them choose again. Otherwise...give them an order number.
+    
+    
+print("""
+Thank you!
+Your order number is: """)
